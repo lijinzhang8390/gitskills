74f99f2049793274d6ec995f45578bbbd2f0d295
lijinzhang <lijinzhang@ddjf.com.cn>
2018-07-10 17:41:40 +0800

    confi..1

--- test02/src/main/java/com/ddjf/ljz/test02/App.java
+++ test02/src/main/java/com/ddjf/ljz/test02/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "This is a test.d" );
+        System.out.println( "This is a test." );
+        System.out.println( "you AND me." );
     }
 }
